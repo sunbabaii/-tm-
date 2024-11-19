c8f5517012a36eab8a4dcd5593cf3cf035e6e338
sunbabaii <100560278+sunbabaii@users.noreply.github.com>
2024-11-19 10:09:00 +0800

    Update init-settings.sh

--- scripts/init-settings.sh
+++ scripts/init-settings.sh
@@ -5,7 +5,7 @@ uci set luci.main.mediaurlbase='/luci-static/argon'
 uci commit luci
 
 # Disable IPV6 ula prefix
-# sed -i 's/^[^#].*option ula/#&/' /etc/config/network
+ sed -i 's/^[^#].*option ula/#&/' /etc/config/network
 
 # Check file system during boot
 # uci set fstab.@global[0].check_fs=1
